6321879062e3fdb451520d043986e654e4294fe1
Alin Mindroc <mindroca@amazon.com>
2017-12-09 17:01:11 +0100

    add google maps support

--- example-server/static-server.js
+++ example-server/static-server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var cors = require('cors')
 
 function StaticServer(port) {
     var server = express();
+    server.use(cors())
     
     server.use('/', express.static(__dirname + '/..'));
+	server.use('/static', express.static(__dirname + '/../static'));
     
     console.log('Open MCT hosted at http://localhost:' + port);
     
